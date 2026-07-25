9de2a952cf5a2b2127092898300653d30b189af5
Valent-p <valent.phiri@gmail.com>
2026-07-25 12:04:08 +0200

    feat: register auth router in FastAPI application

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
+from .api.auth import router as auth_router
 from .api.user import router as user_router
 
 app = FastAPI()
 
+app.include_router(auth_router)
 app.include_router(user_router)
 
 
