194dfd1e6602e7a4138ca518b3f19a9fefb1b97c
Japhet-cyber <jmoyo3691@gmail.com>
2026-07-25 14:45:14 +0200

    Update Login.jsx
    Added form header

--- src/Login.jsx
+++ src/Login.jsx
@@ -15,5 +15,18 @@ function Login() {
         console.log('Email: ', email)
         console.log('Password: ', password)
         alert('Login button clicked check for details')
+
+        return (
+            <div className="login-container">
+                <div className="login-box">
+
+                    //Header part
+                    <div className= "logo">logo</div>
+                    <h1>Welcome to Phindu Assistance</h1>
+
+                    <P>Your personal Phindu assistance suite is ready for you</P>
+                    </div>
+
+                    
         
     }
