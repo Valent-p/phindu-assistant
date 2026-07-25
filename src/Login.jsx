a96bb382248e9252f19fdab80350a8297547201c
Japhet-cyber <jmoyo3691@gmail.com>
2026-07-25 14:09:41 +0200

    Update Login.jsx
    added a login function with constants to store email and password

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,2 +1,7 @@
 import React, { useState } from 'react'
 import './Login.css'
+
+function Login() {
+    // constants for username and password state
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
