99c0cd6f369a50d6b93f699b6c9949fb92e889a4
Japhet-cyber <jmoyo3691@gmail.com>
2026-07-25 14:16:32 +0200

    Update Login.jsx
    created another variable to set hide and display password

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,3 +5,4 @@ function Login() {
     // constants for username and password state
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
